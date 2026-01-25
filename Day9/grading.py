11c01a6728109c7c542994a580ba6d452751f275
guimmamanna <guimmamanna@yahoo.com>
2026-01-25 00:59:05 +0000

    dictionaries

--- Day9/grading.py
+++ Day9/grading.py
@@ -1,1 +1,19 @@
+student_scores = {
+    'Harry': 88,
+    'Ron': 78,
+    'Hermione': 95,
+    'Draco': 75,
+    'Neville': 60
+}
+for student in student_scores:
+    score = student_scores[student]
+    if score <= 70:
+        print(f"{student} Fail")
+    elif 71 <= score <= 80:
+        print(f"{student} Acceptable")
+    elif 81 <= score <= 90:
+        print(f"{student} Exceeds Expectations")
+    else:
+        print(f"{student} Outstanding")
 
+student_grades = {}
